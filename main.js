37a47ad792c9cf2c19bb2e95db2a71f9441406e5
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-03 19:43:51 +0200

    enum in javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+function loop(num) {
+    for (var i = 0; i < num; i++) {
+        console.log(i);
+    }
+    console.log('the last value is: ' + i);
+}
+loop(6);
+var Colors;
+(function (Colors) {
+    Colors["yellow"] = "y";
+    Colors[Colors["blue"] = 5456] = "blue";
+})(Colors || (Colors = {}));
+;
+var font = Colors.yellow;
+console.log(font);

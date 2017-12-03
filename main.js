34b9360713cf8e479020a4255b67527a233c116f
mahmoodahmad100 <mahmood073@gmail.com>
2017-12-03 20:31:21 +0200

    testing altLoop arrow function

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ function loop(num) {
     console.log('the last value is: ' + i);
 }
 loop(6);
+// rewrite loop function (arrow function)
+var altLoop = function (num) {
+    for (var i = 0; i < num; i++) {
+        console.log(i);
+    }
+    console.log('the last value is: ' + i);
+};
+altLoop(6);
+/*******************************************/
 var Colors;
 (function (Colors) {
     Colors["yellow"] = "y";
@@ -13,6 +22,7 @@ var Colors;
 ;
 var font = Colors.yellow;
 console.log(font);
+/*******************************************/
 var msg;
 function print(string) {
     console.log(string);
@@ -20,3 +30,4 @@ function print(string) {
 msg = 'welcome';
 print(msg);
 print(msg);
+/*******************************************/ 
